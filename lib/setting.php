53ab1c5f6ed7bc2c13378a38d1ab358363a8a2e4
novyantsp <novyantsp@gmail.com>
2025-05-10 10:26:13 +0700

    master

---
+++ lib/setting.php
@@ -0,0 +1,18 @@
+<?php
+//<!--
+
+// COPYRIGHT © 2022 RECODE SCTV_DIGITAL //
+
+// PHONE 083191910986 //
+
+// DI LARANG KERAS MEMPERJUALBELIKAN SCR1T INI TANPA IZIN //
+
+// ALL RIGHTS RESERVED //
+
+//-->
+
+//Pengaturan Website
+$config['web'] = array(
+	'url' => 'https://localhost/' // ex: http://domain.com/
+);
+?>
